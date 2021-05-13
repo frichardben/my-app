a2575c78ea09bf3c4b1b608a336ebeefb3bc5996
Richard Ben <frichardben@yahoo.com.br>
2021-05-13 10:23:12 -0300

    add now

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Now
         </a>
-        <button>Aqui</button>
       </header>
     </div>
   );
